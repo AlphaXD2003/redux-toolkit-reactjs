be346a2d5762d008f7d9bc9274303600f5c217e1
Subhamoy Ghosh <alphaxd2023@gmail.com>
2023-10-06 21:07:42 +0530

    first commit

---
+++ todolist/src/App.jsx
@@ -0,0 +1,15 @@
+import AddTodo from './components/addTodo'
+import Todos from './components/todo'
+
+function App() {
+  
+  return (
+    <>
+      <h1>Learn about redux toolkit</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
